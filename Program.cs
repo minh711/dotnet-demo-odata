74f809852206f806db0e6fb3898fde831f7cf387
Trương Duy Minh <minhtd.developer@gmail.com>
2024-10-08 23:49:18 +0700

    build: add OData service

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
 using CustomerService.Models;
+using Microsoft.AspNetCore.OData;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OData.ModelBuilder;
+using demo_odata.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +12,18 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddControllers()
+    .AddOData(options =>
+    {
+        var odataBuilder = new ODataConventionModelBuilder();
+        odataBuilder.EntitySet<Book>("Books");
+        odataBuilder.EntitySet<Address>("Addresses");
+        odataBuilder.EntitySet<Press>("Presses");
+
+        options.Select().Filter().OrderBy().Expand().Count().SetMaxTop(100)
+            .AddRouteComponents("odata", odataBuilder.GetEdmModel());
+    });
 
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
